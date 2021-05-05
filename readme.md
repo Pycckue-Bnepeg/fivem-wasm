d8aa5e9179058d25bba7b2e8733c1aa5efa40ea3
ZOTTCE <zottce@gmail.com>
2021-05-05 05:09:56 +0300

    вспомогатор + комментатор

--- readme.md
+++ readme.md
@@ -1,4 +1,24 @@
 # FiveM WASM runtime
 WASM рантайм для мультиплеера [FiveM](https://fivem.net/)
 
+форк fivem лежит [здесь](https://github.com/zottce/fivem). в нем сделан [компонент](https://github.com/ZOTTCE/fivem/tree/wasm/code/components/citizen-scripting-wasm) на плюсах, который  использует внутри себя этот код
+
 код пиздец, не смотреть ...
+
+## структура
+* [`examples\entry`](examples/entry/) - ну пример (сырой)
+* [`glue`](glue/) - статическая библиотека чтобы засунуть ее в fivem
+* [`runtime`](runtime/) - васм рантайм (wasmtime)
+* [`standalone`](standalone/) - тупа поиграться с рантаймом выше
+
+## сборка
+* компилятор RUST
+* [файвм по приколу собрать](https://github.com/citizenfx/fivem/blob/master/docs/building.md)
+* стыбзить или воспользоваться [скриптом](utils/fivem-build.ps1)
+
+## задачи на будушее
+* `invoke_native`
+* пиздатый вппопер (wrapper) для раста для васм скриптов
+* васм модуль с нативными функциями
+* ?
+* ждать возможность использовать `std::net::TcpSocket`
---
+++ utils/fivem-build.ps1
@@ -0,0 +1,22 @@
+$env:BOOST_ROOT = "E:\sources\c\boost_1_71_0";
+$env:Path += ";C:\python27";
+
+# Push-Location -Path "E:\sources\c\fivem-fork\vendor\fivem-wasm" -StackName FiveMWasm;
+
+# cargo build --release --package cfx-component-glue
+
+# так сказать local dev
+cargo wasi build --package entry --release
+cargo build --package cfx-component-glue --release
+
+Copy-Item "E:\sources\projects\fivem-wasm\target\release\cfx_component_glue.lib" -Destination "E:\sources\c\fivem-fork\vendor\fivem-wasm\target\release\" -Force
+Copy-Item "E:\sources\projects\fivem-wasm\glue\cfx-wasm-runtime.h" -Destination "E:\sources\c\fivem-fork\vendor\fivem-wasm\glue\" -Force
+Copy-Item "E:\sources\projects\fivem-wasm\target\wasm32-wasi\release\entry.wasm" -Destination "E:\sources\c\fivem-fork\code\bin\server\windows\release\resources\main\" -Force
+
+# Pop-Location -StackName FiveMWasm;
+
+Push-Location -Path "E:\sources\c\fivem-fork\code" -StackName FiveMWasm;
+
+.\tools\ci\premake5.exe vs2019 --game=server
+
+Pop-Location -StackName FiveMWasm;
